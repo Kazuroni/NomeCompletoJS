8e22c690190f01ec63cf80e7db1e87cda106b942
Kazuroni <sayajinbr30@gmail.com>
2022-05-26 19:52:43 -0300

    Paulada

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let nome = prompt('Qual é seu nome ?');
+
+let sobrenome = prompt ('Digite seu sobrenome');
+
+alert(`Nome completo; ${nome.toUpperCase()} ${sobrenome}`);
+alert(`Nome catologo; ${sobrenome.toUpperCase()} ${nome}`);
+
+
+
